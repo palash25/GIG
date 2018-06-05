d716ebae8244678919b566927b030c526df96563
Sayan Bhowmik <snbk97@gmail.com>
2018-06-05 17:09:35 +0530

    Added checks for uninitialized directories

--- app/gig.py
+++ app/gig.py
@@ -8,13 +8,21 @@ from .map import MAP
 
 
 class GIG(object):
-    __dest = os.getcwd() + '/.gitignore'
+    __base = os.getcwd()
+    __dest = __base + '/.gitignore'
+    __git  = __base + '/.git'
 
     def __init__(self):
         pass
 
     def gi_not_exists(self,gi_dest):
         if(os.path.isfile(gi_dest)):
+            return 0
+        else:
+            return 1
+
+    def git_not_init(self):
+        if(os.path.isdir(self.__git)):
             return 0
         else:
             return 1
@@ -28,6 +36,14 @@ class GIG(object):
         click.echo(click.style("[SUCC] GITIGNORE generated.",fg='green'))
 
     def generate(self, arg):
+        if(self.git_not_init()):
+            click.echo(click.style("[WARN] Git is not initialized yet",fg='yellow'))
+            if(click.confirm('Do you want to initialize GIT in this directory?', abort=False)):
+                os.system('git init .')
+            else:
+                click.echo(click.style("[INFO] GITIGNORE was not generated.",fg='blue'))
+                quit()
+
         if(self.gi_not_exists(self.__dest)):
             self.process(arg)
         else:
