6cbd59726acd579699cf4d44e6ac3ee7f90e1451
Sayan Bhowmik <snbk97@gmail.com>
2018-06-14 12:13:51 +0530

    Fixed minor issues after #d716eb

--- cli.py
+++ cli.py
@@ -1,7 +1,7 @@
 import click
 import os
 
-
+from app.map import MAP
 from app.gig import GIG
 
 
@@ -11,8 +11,13 @@ def cli(lang):
     ob = GIG()
     if lang == 'None':
         click.echo(click.style("[INFO] Empty GITIGNORE generated,"
-                               "if one doesn\'t already exists.", fg='blue'))
+                               "since lang argument is empty.", fg='blue'))
         os.system('touch .gitignore')
     else:
         ob.generate(lang)
 
+
+@click.command()
+@click.option('--listall', help='Lists all the supported Languages')
+def list_all():
+    print('hello')
